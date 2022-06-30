88e15d0d631f556a6cde4a6d28de76199101944c
Kevin Wright <kevin86wright@gmail.com>
2022-06-30 10:23:06 -0600

    added ssm parameter functionality for storing/fetching api keys

--- services/functions/lambda.ts
+++ services/functions/lambda.ts
@@ -1,20 +1,40 @@
 import fetch from "node-fetch";
-import { S3 } from "aws-sdk";
+import { SSM, S3 } from "aws-sdk";
 
+// aws sdk clients
+const ssm = new SSM();
 const s3 = new S3();
 
+// fetching api keys from SSM
+const apiKey = await ssm
+  .getParameter({ Name: process.env.API_KEY_PARAM_NAME })
+  .promise();
+const apiSecret = await ssm
+  .getParameter({ Name: process.env.API_SECRET_PARAM_NAME })
+  .promise();
+
+// actual values of the parameters
+console.log(apiKey.Parameter.Value);
+console.log(apiSecret.Parameter.Value);
+
+// request config
+const url = "https://swapi.dev/api/people/1/";
+const reqConfig = {
+  method: "GET",
+  headers: { "Content-Type": "application/json" },
+};
+
+// filename of the file in s3
+const filename = "the_filename.json";
+
 export async function main() {
-  // get the json data
-  const res = await fetch("https://swapi.dev/api/people/1/", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
+  // send the request
+  const res = await fetch(url, reqConfig);
+  // get the json
   const data = await res.json();
   console.log(data);
 
-  const filename = "the_filename.json";
-
-  // uploads a file to S3
+  // uploads json into a file in S3
   await s3
     .putObject({
       Bucket: process.env.BUCKET_NAME,
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -4,7 +4,7 @@ import {
   Cron,
   StackContext,
 } from "@serverless-stack/resources";
-import { App } from "aws-cdk-lib";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
 
 export function MyStack({ stack }: StackContext) {
   // destination bucket where JSON will be stored
@@ -17,16 +17,33 @@ export function MyStack({ stack }: StackContext) {
     },
   });
 
+  // mapping SSM parameters to grant lambda ability to read them
+  const apiKey = StringParameter.fromStringParameterName(
+    stack,
+    "apiKey",
+    "/someWebsite/api_key"
+  );
+  const apiSecret = StringParameter.fromStringParameterName(
+    stack,
+    "apiSecret",
+    "/someWebsite/api_secret"
+  );
+
   // the lambda
   const lambda = new Function(stack, "Function", {
     handler: "functions/lambda.main",
     environment: {
       // passing in the bucket name so the s3 sdk can write to bucket
       BUCKET_NAME: bucket.bucketName,
+      API_KEY_PARAM_NAME: apiKey.parameterName,
+      API_SECRET_PARAM_NAME: apiSecret.parameterName,
     },
     // granting lambda read/write permissions on the bucket
     permissions: [bucket],
   });
+
+  apiKey.grantRead(lambda);
+  apiSecret.grantRead(lambda);
 
   // Cron job is an EventBridge Rule that runs the job on a schedule
   new Cron(stack, "Cron", {
